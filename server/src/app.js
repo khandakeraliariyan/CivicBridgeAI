3838a1838a6a473df6b5a7447f54b643c0ab7f87
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-17 19:53:28 +0600

    created the assesment routes and connected with app

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const userRoutes = require("./routes/user.routes");
+const assessmentRoutes = require("./routes/assessment.routes");
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/assessments", assessmentRoutes);
 
 module.exports = app;

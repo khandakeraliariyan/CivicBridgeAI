68a129c144702f9e93e324140ed1a37ec6c2efcd
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-18 11:48:25 +0600

    updated the priority repo and add new function

--- server/src/repositories/priority.repository.js
+++ server/src/repositories/priority.repository.js
@@ -3,9 +3,27 @@ const supabase = require("../config/supabase");
 const createPriorities = async (priorities) => {
     return await supabase
         .from("priorities")
-        .insert(priorities);
+        .insert(priorities)
+        .select();
+};
+
+const getPrioritiesByAssessmentId = async (assessmentId) => {
+    return await supabase
+        .from("priorities")
+        .select("*")
+        .eq(
+            "assessment_id",
+            assessmentId
+        )
+        .order(
+            "priority_order",
+            {
+                ascending: true,
+            }
+        );
 };
 
 module.exports = {
     createPriorities,
+    getPrioritiesByAssessmentId,
 };
